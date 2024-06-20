69f92f35c69f0260372892673a6e724d9306e516
C-killer <124895210+C-killer@users.noreply.github.com>
2024-06-20 15:48:32 +0800

    Update sock_server.cpp

--- sock_server.cpp
+++ sock_server.cpp
@@ -9,16 +9,26 @@ int main() {
     // 监听socket的文件描述符
     // 指向一个 sockaddr 结构体的指针，用于存储连接客户端的地址信息。对客户端地址信息不感兴趣，可以传递 NULL。
     // 一个指向 socklen_t 类型变量的指针，该变量保存 addr 结构体的大小。如果 addr 为 NULL，则 addrlen 也应该为 NULL。
+    
+    /* 使用 accept(fd, NULL, NULL)
     int cliFd = accept(serFd, NULL, NULL);
     if (cliFd < 0) {   
+        print_error("accept error");
+    }
+    */
+
+    //使用 int accept(fd, &addr, &addrlen);
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    int cliFd = accept(serFd, (struct sockaddr *)&client_addr, &client_addr_len);
+    if (cliFd < 0) {
         print_error("accept error");
     }
     std::cout << "Client connected" << std::endl;
 
     //与客户端通信
     char buf[BUFSIZ] = { 0 };
-    bool continuer = true;
-    while (continuer) {
+    while (1) {
         //recv 从指定的sokcet通信
         if (recv(cliFd, buf, BUFSIZ, 0) == -1) {
             print_error("recieve error");
@@ -29,7 +39,7 @@ int main() {
         std::cout << "send>";
         std::cin.getline(buf, BUFSIZ);
         if (strcmp(buf, "exit") == 0) {
-            continuer = false;
+            break;
         }
         if (send(cliFd, buf, BUFSIZ, 0) < 0) {
             print_error("send error");
